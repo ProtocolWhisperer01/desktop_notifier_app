e6cc89a5c0c5a94d508b046f975159d3dbc71807
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-10 23:36:56 +0300

    Started out on the dev of the app.

--- desktop_notifier_app.py
+++ desktop_notifier_app.py
@@ -8,4 +8,15 @@
 #############################################################################
 
 import requests
+from plyer import notification
 
+not_title = 'GREETINGS SIR! HOW IS YOUR MORNING ?'
+not_message = 'From my side everything is well, Hope you have a wonderful day!.'
+
+notification.notify(
+    title = notification_title,  
+    message = notification_message,  
+    app_icon = None,  
+    timeout = 10,  
+    toast = False
+    )
